040b5f0333f62741f41d090b384d677171353dea
Albislan <albislan@hotmail.com>
2020-12-24 10:44:49 -0300

    adicionando funcoes de disparo e movimento da nave

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const yourShip = document.querySelector('.player_shooter');
-const playArea = document.querySelector('#main_play_game');
+const playArea = document.querySelector('#main_play_area');
 
 
 function flyShip(event) {
@@ -23,6 +23,47 @@ function moveUp() {
     }else {
         let position = parseInt(topPosition);
         position -= 50;
-        yourShip.style.top = '${position}px';
+        yourShip.style.top = position + 'px';
     }
 }
+
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "550px") {
+        return;
+    }else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = position + 'px';
+    }
+}
+
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser();
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/disparo.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = xPosition + 'px';
+    newLaser.style.top = yPosition - 5 + 'px';
+    return newLaser;
+}
+
+function moveLaser() {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+        if(xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = xPosition + 8 +'px';
+        }
+    }, 10);
+}
+
+window.addEventListener('keydown', flyShip)
